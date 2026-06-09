22846ed0306e19463863df93cd18e5531e6f2511
kojikoji-rrr <example@mail>
2026-06-09 22:12:02 +0900

    プロジェクト初期化

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('authen_web');
+  protected readonly title = signal('authen-web');
 }
